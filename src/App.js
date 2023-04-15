5e7a0f2cc01a9f9d5dbe897fd516a166ad40a278
Smart Oghenemine <35765212+VinciDemons@users.noreply.github.com>
2023-04-15 12:13:03 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import About from "./components/About";
+import Banner from "./components/Banner";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Services from "./components/Services";
+import Work from "./components/Work";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Banner />
+      <About />
+      <Nav />
+      <Services />
+      <Work />
+      <Contact />
     </div>
   );
 }
